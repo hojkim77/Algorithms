33e0ff4d14c2e4a8d57fd33b24eefe1ca4b5a2bc
hojkim77 <rlaghwnd4534@gmail.com>
2023-01-08 01:25:35 +0900

    dp

--- dp14.py
+++ dp14.py
@@ -9,7 +9,7 @@ elif n == 2:
 else:
     dp[0] = max(dp[0], dp[0] + dp[1])
 
-    for i in range(1,n - 1):
+    for i in range(1, n - 1):
         if arr[i] >= 0:
             dp[i] = max(dp[i - 1] + dp[i + 1], dp[i - 1], arr[i], arr[i] + dp[i + 1])
         if arr[i] < 0:
