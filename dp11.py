11a485a1166df42e382c3fce238f42da5c8a3fd6
hojkim77 <rlaghwnd4534@gmail.com>
2023-01-07 18:44:24 +0900

    dp

--- dp11.py
+++ dp11.py
@@ -1,10 +1,11 @@
 n = int(input())
 arr = list(map(int, input().split()))
-dp = [1] * n
-
+dp = arr[:]
+print(dp)
 for i in range(n):
     for j in range(i):
         if arr[j] < arr[i]:
-            dp[i] = max(dp[i], dp[j] + 1)
-            
-print(max(dp))
+            dp[i] = max(dp[i], dp[j] + arr[i])
+
+print(dp)
+print(max(max(dp), max(arr)))
--- dp12.py
+++ dp12.py
@@ -4,7 +4,7 @@ dp = [1] * n
 
 for i in range(n):
     for j in range(i):
-        if arr[j] < arr[i]:
+        if arr[j] > arr[i]:
             dp[i] = max(dp[i], dp[j] + 1)
             
 print(max(dp))
